5c2eac80ef1a7b6a13401a0282863b7abf4b6998
SpencerLiege <66042763+SpencerLiege@users.noreply.github.com>
2024-06-14 15:01:05 +0100

    Update app.js
    Cors update

--- Backend/app.js
+++ Backend/app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
 app.use(cors({
-  origin: 'http://your-frontend-domain.com',
+  origin: 'http://localhost:5173',
   credentials: true, // This allows the browser to include cookies in the requests
 }));
 
